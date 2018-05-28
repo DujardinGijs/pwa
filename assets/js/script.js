94657d0a33a49785b3b88ed7f1abaa449ac9410e
gijs.dujardin <gijs.dujardin@student.howest.be>
2018-05-29 00:21:51 +0200

    tets

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,6 +156,28 @@ function verwijder() {
         console.log("failllllll " + e);
     };
 }
+let deferredPrompt;
+function homescreen() {
+    e.preventDefault();
+    deferredPrompt = e;
+    btnAdd.style.display = 'block';
+    btnAdd.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        btnAdd.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+            .then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+    });
+}
 function init() {
     $("#off").hide();
     serviceworker();
@@ -163,6 +185,7 @@ function init() {
     terug();
     window.addEventListener('offline', handleConnectionChange);
     window.addEventListener('online', handleConnectionChange);
+    window.addEventListener('beforeinstallprompt',homescreen);
     $("#nieuw").on('click', selecteer);
     $('#selecteer').on('submit', nieuwPunt);
     $('#nieuwPunt').on('submit', slaOp);
